c153ea0d2f558812eaaef0f98b1223cd2525d511
HinnMachache <bennyhinn253.bh@gmail.com>
2024-10-09 23:21:42 +0300

    Restructuring Project Files

---
+++ main_app/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Flask Main App
+"""
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'  # Enable @login_required
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASSCODE')
+mail = Mail(app)
+
+from main_app import routes
