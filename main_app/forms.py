76d1aad07578aa812fdf6e9f0c8dfb673163041a
HinnMachache <bennyhinn253.bh@gmail.com>
2024-10-09 23:00:28 +0300

    Added Models and Forms

---
+++ main_app/forms.py
@@ -0,0 +1,62 @@
+""" Form Classes
+"""
+from flask_wtf import FlaskForm
+from wtforms import (StringField, EmailField, PasswordField, IntegerField,
+                     TelField, DateField, SubmitField, RadioField)
+from flask_wtf.file import FileField, FileAllowed
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from main_app.models import User
+
+
+
+class ApplicationForm(FlaskForm):
+    """ Registration Form"""
+    surname = StringField('Last Name', validators=[DataRequired(), Length(min=2, max=25)])
+    other_names = StringField('First Name', validators=[DataRequired(), Length(min=2, max=50)])
+    dob = DateField('Date of Birth', validators=[DataRequired()])
+    id_number = IntegerField('ID Number', validators=[DataRequired()])
+    phone_number = TelField('Phone Number', validators=[DataRequired(), Length(min=10)])
+    address = StringField('Address', validators=[DataRequired(), Length(min=2, max=30)])
+    postal_code = IntegerField('Postal Code', validators=[DataRequired()])
+    passport_photo = FileField('Passport Photo', validators=[FileAllowed(['jpg, jpeg'])])
+    copy_photo = FileField('Copy of ID Card/Passport', validators=[FileAllowed(['jpg, jpeg'])])
+    gender = RadioField('Gender', choices=[('male', 'Male'), ('female', 'Female'), ('other', 'Other')])
+    submit = SubmitField('Save Info')
+    
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=30)])
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("Username already taken. Please choose a different one.")
+        
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("Email already taken. Please choose a different one.")    
+
+class LoginForm(FlaskForm):
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class ResetPasswordRequestForm(FlaskForm):
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Reset Password')
+
+        
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError("There is no account with that email. You must register first.")  
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
---
+++ main_app/models.py
@@ -0,0 +1,61 @@
+from main_app import db, login_manager, app
+import jwt
+from time import time
+from flask_login import UserMixin   # Manage sessions
+
+
+@login_manager.user_loader  # Reloading the user from user id stored in the session
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    user_id = db.Column(db.Integer, primary_key=True)
+    personal_data = db.relationship('User_personalData', back_populates='user', uselist=False)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    personal_data_submitted = db.Column(db.Boolean, default=False)
+    user_profile = db.Column(db.String(20), nullable=False, default="profile.png")
+
+    def get_id(self):
+        return self.user_id
+    
+    def get_reset_password_token(self, expires_in=1800):
+        return jwt.encode({
+            'reset_password': self.user_id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256'
+        )
+    
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            usr_id = jwt.decode(token, app.config['SECRET_KEY'],
+                                algorithms=['HS256'])['reset_password']
+        except:
+            return None
+        return User.query.get(usr_id)
+    
+    
+    def __repr__(self) -> str:
+        return f"{self.username} : {self.email} : {self.user_profile}"
+    
+
+class User_personalData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), index=True)  # Foreign Key
+    user = db.relationship('User', back_populates='personal_data', lazy=True)
+    surname = db.Column(db.String(30), unique=True, nullable=False)
+    other_names = db.Column(db.String(120), unique=True, nullable=False)
+    dob = db.Column(db.String(20), nullable=False)
+    id_number = db.Column(db.Integer, unique=True, nullable=False)
+    telephone_no = db.Column(db.String(20), unique=True, nullable=False)
+    address = db.Column(db.String(50), nullable=False)
+    postal_code= db.Column(db.Integer, nullable=False)
+    gender= db.Column(db.String(5), nullable=False)
+    passport_photo = db.Column(db.String(20), nullable=False, default="profile.png")
+    copy_id = db.Column(db.String(20), nullable=False, default="profile.png")
+    
+
+    def __repr__(self) -> str:
+        return f"{self.surname} : {self.other_names} : {self.passport_photo}"
